3d3b594eacbc6e0b817e003904746ba7670bf184
Fraser Yang-Turner <Fraser.YT@outlook.com>
2023-12-30 13:35:05 +0000

    Day 6 Part 2

--- 2023/Day6/Day6.py
+++ 2023/Day6/Day6.py
@@ -1,15 +1,14 @@
 f = open("Data.txt", "r")
 data = f.readlines()
 
-times = data[0].split(":")[1].rstrip("\n").split()
-distance = data[1].split(":")[1].rstrip("\n").split()
+time = data[0].split(":")[1].strip()
+distance = data[1].split(":")[1].strip()
 
-output = 1
-for time in range(len(times)):
-    total = 0
-    for charge in range(int(times[time]) + 1):
-        if charge * (int(times[time]) - charge) > int(distance[time]):
-            total += 1
-    output *= total
+print(time, distance)
 
-print(output)
+total = 0
+for charge in range(int(time) + 1):
+    if charge * (int(time) - charge) > int(distance):
+        total += 1
+
+print(total)
